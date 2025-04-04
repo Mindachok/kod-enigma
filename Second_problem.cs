aa1a00dcb063a64492151871445c1a5ef3daefd7
Mindachok <i.mindachok@gmail.com>
2025-04-04 21:09:19 +0400

    second commit

---
+++ Second_problem.cs
@@ -0,0 +1,18 @@
+﻿namespace KOD_Enigma;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Привет, мир!");
+        Console.WriteLine("Введите число");
+        int a = int.Parse(Console.ReadLine());
+        if (a % 2 == 0) {
+            Console.WriteLine("Введённое число чётное");
+        }
+        else {
+            Console.WriteLine("Введённое число нечётное");
+        }
+
+    }
+}
